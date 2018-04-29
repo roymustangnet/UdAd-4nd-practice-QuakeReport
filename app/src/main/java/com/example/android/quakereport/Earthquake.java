24cb2848d3a1bd8794e88642eb1003583f7dbe23
Roy Mustang <roy.mustang.net@gmail.com>
2018-04-30 07:44:15 +0800

    beautiful

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -1,17 +1,17 @@
 package com.example.android.quakereport;
 
 public class Earthquake {
-    private String mMagnitude;
+    private double mMagnitude;
     private String mLocation;
     private long mTimeInMilliseconds;
 
-    public Earthquake(String magnitude, String location, long timeInMilliseconds){
+    public Earthquake(double magnitude, String location, long timeInMilliseconds){
         this.mMagnitude = magnitude;
         this.mLocation = location;
         this.mTimeInMilliseconds = timeInMilliseconds;
     }
 
-    public String getMagnitude(){return this.mMagnitude;}
+    public double getMagnitude(){return this.mMagnitude;}
     public String getLocation(){return this.mLocation;}
     public long getTimeInMilliseconds(){ return this.mTimeInMilliseconds; }
 }
--- app/src/main/java/com/example/android/quakereport/EarthquakeListAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeListAdapter.java
@@ -7,29 +7,66 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+import android.graphics.drawable.GradientDrawable;
+import android.support.v4.content.ContextCompat;
+
 public class EarthquakeListAdapter extends ArrayAdapter<Earthquake> {
+    private static final String LOCATION_SEPARATOR = " of ";
+
     public EarthquakeListAdapter(Context context, ArrayList<Earthquake> earthquakes){
         super(context, 0, earthquakes);
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        Earthquake earthquake = getItem(i);
+        String primaryLocation;
+        String locationOffset;
+
+        Earthquake currentEarthquake = getItem(i);
         View listItemView = view;
         if(listItemView == null){
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.earthquake_list_item, viewGroup, false);
         }
+
+
+
+        // 格式化震级使其显示一位小数
+        String formattedMagnitude = formatMagnitude(currentEarthquake.getMagnitude());
         TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magnitude);
-        magnitudeTextView.setText(earthquake.getMagnitude());
-        TextView locationTextView = (TextView) listItemView.findViewById(R.id.location);
-        locationTextView.setText(earthquake.getLocation());
+        magnitudeTextView.setText(formattedMagnitude);
+
+        // 为震级圆圈设置正确的背景颜色。
+        // 从 TextView 获取背景，该背景是一个 GradientDrawable。
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeTextView.getBackground();
+
+        // 根据当前的地震震级获取相应的背景颜色
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+
+        // 设置震级圆圈的颜色
+        magnitudeCircle.setColor(magnitudeColor);
+
+        String originalLocation = currentEarthquake.getLocation();
+        if (originalLocation.contains(LOCATION_SEPARATOR)) {
+            String[] parts = originalLocation.split(LOCATION_SEPARATOR);
+            locationOffset = parts[0] + LOCATION_SEPARATOR;
+            primaryLocation = parts[1];
+        } else {
+            locationOffset = getContext().getString(R.string.near_the);
+            primaryLocation = originalLocation;
+        }
+        TextView primaryLocationView = (TextView) listItemView.findViewById(R.id.primary_location);
+        primaryLocationView.setText(primaryLocation);
+
+        TextView locationOffsetView = (TextView) listItemView.findViewById(R.id.location_offset);
+        locationOffsetView.setText(locationOffset);
 
         // 根据地震时间（以毫秒为单位）创建一个新的 Date 对象
-        Date dateObject = new Date(earthquake.getTimeInMilliseconds());
+        Date dateObject = new Date(currentEarthquake.getTimeInMilliseconds());
 
         // 找到视图 ID 为 date 的 TextView
         TextView dateView = (TextView) listItemView.findViewById(R.id.date);
@@ -62,5 +99,51 @@ public class EarthquakeListAdapter extends ArrayAdapter<Earthquake> {
         SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
         return timeFormat.format(dateObject);
     }
+    /**
+     * 从十进制震级值返回格式化后的仅显示一位小数的震级字符串
+     * （如“3.2”）。
+     */
+    private String formatMagnitude(double magnitude) {
+        DecimalFormat magnitudeFormat = new DecimalFormat("0.0");
+        return magnitudeFormat.format(magnitude);
+    }
 
+    private int getMagnitudeColor(double magnitude) {
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
+    }
 }
